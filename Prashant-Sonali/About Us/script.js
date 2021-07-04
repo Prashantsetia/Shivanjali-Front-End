13c854aba45345a446064d9b13054fbee6be72d6
Prashantsetia <66926991+Prashantsetia@users.noreply.github.com>
2021-07-05 02:40:54 +0530

    About Us Page With Final Changes

---
+++ Prashant-Sonali/About Us/script.js
@@ -0,0 +1,28 @@
+$('.count').counterUp({
+    delay: 20,
+    time: 2000
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
